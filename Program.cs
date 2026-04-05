a4147850282632971fbc5a09e925050e0737ea2d
Ma1c <retir1603@gmail.com>
2026-04-05 14:23:45 +0300

    added random chars writer

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿while (true) {
+	Random rand = new Random();
+	string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	char randomChar = allowedChars[rand.Next(0, allowedChars.Length)];
+	Console.Clear();
+	Console.Write(randomChar);
+	Console.Write(randomChar);
+	Console.Write(randomChar);
+}
